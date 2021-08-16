b43395bd6d0e1433a5becd16ba53b71e17a7721f
Julien Piron <56518809+techjpiron@users.noreply.github.com>
2021-08-16 08:40:52 +0200

    feat: remove

--- components/ConceptGrp.js
+++ components/ConceptGrp.js
@@ -1,5 +1,5 @@
 const ConceptGrp = ({ children }) => (
-  <div className="my-4 p-8 rounded-xl bg-gray-800 text-white text-opacity-90 shadow-2xl">
+  <div className="relative my-4 pt-16 pb-8 px-8 rounded-xl bg-gray-800 text-white text-opacity-90 shadow-2xl">
     {children}
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import { useLocalStorage } from "react-use";
+
+import produce from "immer";
+
 import Nav from "../components/Nav";
 import UploadButton from "../components/UploadButton";
 import ConceptGrp from "../components/ConceptGrp";
@@ -7,33 +10,59 @@ import TermGrp from "../components/TermGrp";
 import Term from "../components/Term";
 import Field from "../components/Field";
 import Meta from "../components/Meta";
+//import DumpJson from "../components/DumpJson";
+
 import toArray from "../lib/toArray";
 
-const IndexPage = () => {
-  const [file, setFile] = useLocalStorage("file");
+import { HiTrash } from "react-icons/hi";
 
-  const conceptGrp = file?.mtf?.conceptGrp;
+const DeleteButton = ({ remove }) => {
+  return (
+    <button
+      onClick={remove}
+      className="absolute right-8 top-4 p-2 bg-gray-100 rounded-full"
+    >
+      <span className="sr-only">Delete</span>
+      <HiTrash className="h-4 w-4 text-gray-800 transform hover:scale-105" />
+    </button>
+  );
+};
+
+const IndexPage = () => {
+  const [data, setData] = useLocalStorage("data");
+  const conceptGrp = data?.mtf?.conceptGrp;
+
+  const remove = (id) => () => {
+    setData(
+      produce((draft) => {
+        draft.mtf.conceptGrp = data.mtf.conceptGrp.filter(
+          (concept) => concept.concept !== id
+        );
+      })
+    );
+  };
 
   return (
     <div className="p-4 bg-gray-200 min-h-screen">
       <Nav className="w-full flex justify-between items-center bg-gray-500">
         <h1 className="text-3xl">Terminologist</h1>
-        <UploadButton setFile={setFile} />
+        <UploadButton setFile={setData} />
       </Nav>
       {conceptGrp ? (
-        conceptGrp.map(({ concept, languageGrp, transacGrp }) => (
-          <ConceptGrp key={concept}>
-            <Field name="id">{concept}</Field>
+        conceptGrp.map(({ concept: id, languageGrp, transacGrp }) => (
+          <ConceptGrp key={id}>
+            <DeleteButton remove={remove(id)} />
+            <Field name="id">{id}</Field>
             {toArray(transacGrp).map(({ transac: { value, _type }, date }) => (
               <Meta
                 value={value}
                 type={_type}
                 date={date}
-                key={`${value}-${_type}`}
+                key={`${id}${value}${_type}`}
               />
             ))}
             {languageGrp.map(({ language, termGrp }) => (
-              <LanguageGrp key={language._type}>
+              <LanguageGrp key={`${id}${language._type}`}>
                 <Field name="Language">{language._type}</Field>
                 {toArray(termGrp).map(({ term, transacGrp, descripGrp }) => (
                   <TermGrp key={term}>
@@ -51,7 +80,7 @@ const IndexPage = () => {
                           value={value}
                           type={_type}
                           date={date}
-                          key={`${value}-${_type}`}
+                          key={`${id}${value}${_type}`}
                         />
                       )
                     )}
